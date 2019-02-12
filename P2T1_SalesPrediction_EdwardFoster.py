4a2775bacf5ec7e405a12646d668524c96beaeb0
foster8319 <47574937+foster8319@users.noreply.github.com>
2019-02-12 14:31:21 -0500

    Add files via upload
    Sales Prediction

---
+++ P2T1_SalesPrediction_EdwardFoster.py
@@ -0,0 +1,12 @@
+# This program will display profit thats been made off sales
+# 02/12/2019
+# CTI-110 P2T1 - Sales Prediction
+# Edward Foster
+#
+#.23 represent 23 percent
+
+totalSales=float(input("Enter the projeted sales: "))
+
+profit = totalSales * 0.23
+
+print("The profit is $", format(profit, ',.2f'))
